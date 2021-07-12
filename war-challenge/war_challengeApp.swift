83a31c4246709bfcd104e89653fed53c750ec9e6
Arijana Besirovic <arijana772@gmail.com>
2021-07-12 14:57:17 +0200

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by MacBook Pro on 19. 6. 2021..
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
